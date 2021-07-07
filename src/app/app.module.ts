aebd35552e329b5f6921b07cd65d54767958257b
Laura <pedroscampoy@gmail.com>
2021-07-07 13:50:09 +0200

    pieDataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,13 @@ import { AppComponent } from './app.component';
 import { PieCustomDataComponent } from './components/pie-custom-data/pie-custom-data.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 
+//Providers
+import { PieDataService } from './services/pie-data.service';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    PieCustomDataComponent,
-    PieChartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PieCustomDataComponent, PieChartComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [PieDataService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import * as D3 from 'd3';
+import { IData } from 'src/app/services/pie-data.interface';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.scss']
+  styleUrls: ['./pie-chart.component.scss'],
 })
-export class PieChartComponent implements OnInit {
+export class PieChartComponent implements AfterViewInit {
+  @ViewChild('containerPieChart') element: ElementRef;
 
-  constructor() { }
+  private host: D3.Selection<d3.BaseType, {}, d3.BaseType, any>;
+  private svg: D3.Selection<d3.SVGElement, {}, d3.BaseType, any>;
+  private width: number;
+  private height: number;
+  private htmlElement: HTMLElement;
+  private pieData: IData[];
 
-  ngOnInit(): void {
-  }
+  constructor(private dataService: PieDataService) {}
 
+  ngOnInit(): void {}
 }
