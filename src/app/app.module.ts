0451dbeb3cfeefe1d8e9de069a0658a0ebf80071
Laura <pedroscampoy@gmail.com>
2021-07-08 12:47:22 +0200

    bootstrap added and tutorial completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +12,7 @@ import { PieDataService } from './services/pie-data.service';
 
 @NgModule({
   declarations: [AppComponent, PieCustomDataComponent, PieChartComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [PieDataService],
   bootstrap: [AppComponent],
 })
